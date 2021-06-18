ec672a975908fefef0992413b5d24565ae6943db
Rose <r.d.jeanlouis01@gmail.com>
2021-06-17 20:16:26 -0400

    First commit 

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,30 +1,51 @@
 import React from "react";
 
+// This component takes one prop: handleLogin which is a function
+
+// You'll find two inputs in this component: <input type="text"> and <input type="password">. 
+//Make this a controlled component by adding the necessary attributes to these inputs. 
+//The input values should be saved to the component's state on every change.
+
+
 class LoginForm extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      value: {
+        username: '',
+        password: ''
+      }
+    };
+  }
+
+  handleChange = (event) => {
+    this.setState({value: event.target.value})
+  }
+
+  handleLogin = (event) => {
+    event.preventDefault();
   }
 
   render() {
     return (
-      <form>
+      <form onSubmit={this.handleLogin}>
         <div>
           <label>
             Username
-            <input id="username" name="username" type="text" />
+            <input id="username" name="username" type="text" value={this.state.username} onChange= {this.handleChange}/>
           </label>
         </div>
         <div>
           <label>
             Password
-            <input id="password" name="password" type="password" />
+            <input id="password" name="password" type="password" value={this.state.username} onChange= {this.handleChange}/>
           </label>
         </div>
         <div>
           <button type="submit">Log in</button>
         </div>
+        
       </form>
     );
   }
--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -1,17 +1,32 @@
 import React from "react";
 
+//1. This component takes one prop: maxChars which is a number 
+
+//2. You'll find an <input type="text"> in this component. Make this a controlled component by adding the attributes to the <input> element. 
+//Its value should be saved in the component's state and should update on every change to the input.
+
+//3. Show the remaining characters in the component. I.E subtract values typed from total values.
+
 class TwitterMessage extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      value: ''
+    };
+  }
+
+  handleChange = (event) => {
+    this.setState({ value: event.target.value })
   }
 
   render() {
+    let totalChar = this.props.maxChars - this.state.value.length
     return (
       <div>
+        {totalChar}
         <strong>Your message:</strong>
-        <input type="text" name="message" id="message" />
+        <input type="text" name="message" id="message" value={this.state.value} onChange={this.handleChange} />
       </div>
     );
   }
